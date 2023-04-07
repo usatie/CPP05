db1e3cfb323cda0d459075a075acd4446cc39bc2
Shun Usami <usatie@gmail.com>
2023-04-07 16:15:53 +0900

    :sparkles: [ex01] Add debug mode

--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -12,7 +12,9 @@ Form::Form()
       _signed(false),
       _gradeToSign(LOWEST_GRADE),
       _gradeToExecute(LOWEST_GRADE) {
+#if DEBUG
   std::cout << "[ Form Default constructor called ]" << std::endl;
+#endif
 }
 
 // Constructor
@@ -21,12 +23,16 @@ Form::Form(const Form& f)
       _signed(f._signed),
       _gradeToSign(f._gradeToSign),
       _gradeToExecute(f._gradeToExecute) {
+#if DEBUG
   std::cout << "[ Form Copy constructor called ]" << std::endl;
+#endif
 }
 
 // Copy assignment operator
 Form& Form::operator=(const Form& f) {
+#if DEBUG
   std::cout << "[ Form Copy assignment operator called ]" << std::endl;
+#endif
   if (this != &f) {
     _signed = f._signed;
   }
@@ -34,7 +40,11 @@ Form& Form::operator=(const Form& f) {
 }
 
 // Destructor
-Form::~Form() { std::cout << "[ Form destructor called ]" << std::endl; }
+Form::~Form() {
+#if DEBUG
+  std::cout << "[ Form destructor called ]" << std::endl;
+#endif
+}
 
 /* ==============================================
  *                    Member functions
@@ -45,7 +55,9 @@ Form::Form(std::string const& name, int gradeToSign, int gradeToExecute)
       _signed(false),
       _gradeToSign(gradeToSign),
       _gradeToExecute(gradeToExecute) {
+#if DEBUG
   std::cout << "[ Form Constructor called ]" << std::endl;
+#endif
   // Check if grade is valid
   // Smaller number means higher grade
   if (_gradeToSign < HIGHEST_GRADE) {
@@ -62,6 +74,9 @@ Form::Form(std::string const& name, int gradeToSign, int gradeToExecute)
 
 // Member functions
 void Form::beSigned(Bureaucrat const& b) {
+#if DEBUG
+  std::cout << "[ Form beSigned called ]" << std::endl;
+#endif
   if (b.getGrade() <= _gradeToSign) {
     _signed = true;
   } else {
@@ -72,28 +87,67 @@ void Form::beSigned(Bureaucrat const& b) {
 /* ==============================================
  *                    Getters
  * ============================================== */
-std::string Form::getName() const { return _name; }
+std::string Form::getName() const {
+#if DEBUG
+  std::cout << "[ Form getName called ]" << std::endl;
+#endif
+  return _name;
+}
 
-bool Form::getSigned() const { return _signed; }
+bool Form::getSigned() const {
+#if DEBUG
+  std::cout << "[ Form getSigned called ]" << std::endl;
+#endif
+  return _signed;
+}
 
-int Form::getGradeToSign() const { return _gradeToSign; }
+int Form::getGradeToSign() const {
+#if DEBUG
+  std::cout << "[ Form getGradeToSign called ]" << std::endl;
+#endif
 
-int Form::getGradeToExecute() const { return _gradeToExecute; }
+  return _gradeToSign;
+}
+
+int Form::getGradeToExecute() const {
+#if DEBUG
+  std::cout << "[ Form getGradeToExecute called ]" << std::endl;
+#endif
+
+  return _gradeToExecute;
+}
 
 /* ==============================================
  *                    Exceptions
  * ============================================== */
 // GradeTooHighException
 Form::GradeTooHighException::GradeTooHighException() throw()
-    : _description("Grade is too high") {}
+    : _description("Grade is too high") {
+#if DEBUG
+  std::cout << "[ Form GradeTooHighException Default constructor called ]"
+            << std::endl;
+#endif
+}
 
-Form::GradeTooHighException::~GradeTooHighException() throw() {}
+Form::GradeTooHighException::~GradeTooHighException() throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooHighException destructor called ]" << std::endl;
+#endif
+}
 
 const char* Form::GradeTooHighException::what() const throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooHighException what called ]" << std::endl;
+#endif
+
   return _description.c_str();
 }
 
 Form::GradeTooHighException::GradeTooHighException(int grade) throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooHighException Constructor called ]" << std::endl;
+#endif
+
   std::ostringstream oss;
   oss << "Grade is too high: " << grade;
   _description = oss.str();
@@ -101,15 +155,31 @@ Form::GradeTooHighException::GradeTooHighException(int grade) throw() {
 
 // GradeTooLowException
 Form::GradeTooLowException::GradeTooLowException() throw()
-    : _description("Grade is too low") {}
+    : _description("Grade is too low") {
+#if DEBUG
+  std::cout << "[ Form GradeTooLowException Default constructor called ]"
+            << std::endl;
+#endif
+}
 
-Form::GradeTooLowException::~GradeTooLowException() throw() {}
+Form::GradeTooLowException::~GradeTooLowException() throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooLowException destructor called ]" << std::endl;
+#endif
+}
 
 const char* Form::GradeTooLowException::what() const throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooLowException what called ]" << std::endl;
+#endif
+
   return _description.c_str();
 }
 
 Form::GradeTooLowException::GradeTooLowException(int grade) throw() {
+#if DEBUG
+  std::cout << "[ Form GradeTooLowException Constructor called ]" << std::endl;
+#endif
   std::ostringstream oss;
   oss << "Grade is too low: " << grade;
   _description = oss.str();
